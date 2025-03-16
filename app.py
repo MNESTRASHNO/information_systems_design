325596bf289fb9b15468931c2de28d5f5b8bc9f7
MNESTRASHNO <123@123.ru>
2025-03-16 11:36:07 +0000

    after designing the plan

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route("/index")
+def starting_page():
+
+    return """
+    <html>
+    <head>
+    <p>First commit </p>
+    </head>
+    </html>
+    """
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001)
+
+
+from app import app
